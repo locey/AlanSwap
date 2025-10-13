bc6578a2c42beaa1ee36a36fcc0447639442128a
yuyue3 <yy18825237023@126.com>
2025-10-13 22:28:57 +0800

    入口文件添加WagmiProvider包裹层等

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,37 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  RainbowKitProvider,
+  darkTheme,
+} from '@rainbow-me/rainbowkit'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+import { WagmiProvider, http, createConfig } from "wagmi";
+import { mainnet, polygon, arbitrum, localhost } from "wagmi/chains";
+
+const config = createConfig({
+  chains: [mainnet, polygon, arbitrum, localhost],
+  transports: {
+    [mainnet.id]: http(),
+    [polygon.id]: http(),
+    [arbitrum.id]: http(),
+    [localhost.id]: http("http://127.0.0.1:8545"),
+  },
+});
+
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider theme={darkTheme()}>
+          <App />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   </React.StrictMode>,
 )
